1898bd9d8ec38f331963986f147e94b60e152f8b
andreyshmelev <andrshmel@gmail.com>
2022-05-12 19:21:41 +0300

    теперь норм. можно отправлять

--- ConsoleAppG/Program.cs
+++ ConsoleAppG/Program.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 public class Program
 {
-
-    public static bool strokaIliStolbeccheck(char[] stroka)
-    {
-        return true;
-    }
-
     public struct Line
     {
-
         public int hX = 0;
         public int h0 = 0;
-
         public Line(int howManyCrosses, int howManyZeros)
         {
             hX = howManyCrosses;
@@ -22,222 +14,219 @@ public class Program
 
     public static void Main(string[] args)
     {
-        // using (StreamReader file = new StreamReader("C:\\Users\\Brux\\Downloads\\testG\\tests\\05"))
+        var countDataSets = int.Parse(Console.ReadLine());
         {
-            var countDataSets = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countDataSets; i++)
             {
-                //console.writeLine("countDataSets  " + countDataSets);
 
-                for (int i = 0; i < countDataSets; i++)
+                if (string.IsNullOrEmpty(Console.ReadLine()))
+
                 {
+                    List<Line> strokiList = new List<Line>();
+                    List<Line> stolbciList = new List<Line>();
 
-                    if (string.IsNullOrEmpty(Console.ReadLine()))
+                    bool result = true;
 
+                    char[,] gameAray = new char[3, 3];
+
+                    Line stolb0 = new Line(0, 0);
+                    Line stolb1 = new Line(0, 0);
+                    Line stolb2 = new Line(0, 0);
+                    Line diagonal1 = new Line(0, 0);
+                    Line diagonal2 = new Line(0, 0);
+
+                    var indexfordiagonal = 0;
+                    for (int row = 0; row < 3; row++)
                     {
-                        List<Line> strokiList = new List<Line>();
-                        List<Line> stolbciList = new List<Line>();
+                        string inputsrt = (Console.ReadLine());
 
-                        bool result = true;
+                        // проверяем каждую строку и создаем структуру строки
 
-                        char[,] gameAray = new char[3, 3];
+                        Line linenew = new Line(0, 0);
 
-                        Line stolb0 = new Line(0, 0);
-                        Line stolb1 = new Line(0, 0);
-                        Line stolb2 = new Line(0, 0);
-
-                        Line diagonal1 = new Line(0, 0);
-                        Line diagonal2 = new Line(0, 0);
-
-                        var indexfordiagonal = 0;
-                        for (int row = 0; row < 3; row++)
+                        for (int column = 0; column < 3; column++)
                         {
-                            string inputsrt = (Console.ReadLine());
 
-                            // проверяем каждую строку и создаем структуру строки
+                            char inputchar = inputsrt[column];
+                            gameAray[row, column] = inputchar;
 
-                            Line linenew = new Line(0, 0);
-
-                            for (int column = 0; column < 3; column++)
+                            switch (inputchar)
                             {
+                                case 'X':
+                                    linenew.hX++;
 
-                                char inputchar = inputsrt[column];
-                                gameAray[row, column] = inputchar;
+                                    if (column == 0)
+                                    {
+                                        stolb0.hX++;
+                                    }
+                                    else
+                                    if (column == 1)
+                                    {
+                                        stolb1.hX++;
+                                    }
+                                    else
+                                    if (column == 2)
+                                    {
+                                        stolb2.hX++;
+                                    }
 
-                                // проверка диагонали
+                                    // если слева сверху вправо вниз диагональ
+                                    if (indexfordiagonal == 0 || indexfordiagonal == 8)
+                                    {
+                                        diagonal1.hX++;
+                                    }
 
-                                if ((column == 0) && (row == 0))
-                                {
+                                    // если справа сверху влево вниз диагональ
+                                    if (indexfordiagonal == 2 || indexfordiagonal == 6)
+                                    {
+                                        diagonal2.hX++;
+                                    }
 
-                                }
+                                    // если по центру ячейка
+                                    if (indexfordiagonal == 4)
+                                    {
 
-                                switch (inputchar)
-                                {
-                                    case 'X':
-                                        linenew.hX++;
+                                        diagonal1.hX++;
+                                        diagonal2.hX++;
+                                    }
 
-                                        if (column == 0)
-                                        {
-                                            stolb0.hX++;
-                                        }
-                                        else
-                                        if (column == 1)
-                                        {
-                                            stolb1.hX++;
-                                        }
-                                        else
-                                        if (column == 2)
-                                        {
-                                            stolb2.hX++;
-                                        }
+                                    break;
 
-                                        // если слева сверху вправо вниз диагональ
-                                        if (indexfordiagonal == 0 || indexfordiagonal == 8)
-                                        {
-                                            diagonal1.hX++;
-                                        }
+                                case '0':
+                                    linenew.h0++;
+                                    if (column == 0)
+                                    {
+                                        stolb0.h0++;
+                                    }
+                                    else
+                                    if (column == 1)
+                                    {
+                                        stolb1.h0++;
+                                    }
+                                    else
+                                    if (column == 2)
+                                    {
+                                        stolb2.h0++;
+                                    }
 
-                                        // если справа сверху влево вниз диагональ
-                                        if (indexfordiagonal == 2 || indexfordiagonal == 6)
-                                        {
-                                            diagonal2.hX++;
-                                        }
+                                    // если слева сверху вправо вниз диагональ
+                                    if (indexfordiagonal == 0 || indexfordiagonal == 8)
+                                    {
+                                        diagonal1.h0++;
+                                    }
 
-                                        // если по центру ячейка
-                                        if (indexfordiagonal == 4)
-                                        {
+                                    // если справа сверху влево вниз диагональ
+                                    if (indexfordiagonal == 2 || indexfordiagonal == 6)
+                                    {
+                                        diagonal2.h0++;
+                                    }
 
-                                            diagonal1.hX++;
-                                            diagonal2.hX++;
-                                        }
+                                    // если по центру ячейка
+                                    if (indexfordiagonal == 4)
+                                    {
 
-                                        break;
+                                        diagonal1.h0++;
+                                        diagonal2.h0++;
+                                    }
 
-                                    case '0':
-                                        linenew.h0++;
-                                        if (column == 0)
-                                        {
-                                            stolb0.h0++;
-                                        }
-                                        else
-                                        if (column == 1)
-                                        {
-                                            stolb1.h0++;
-                                        }
-                                        else
-                                        if (column == 2)
-                                        {
-                                            stolb2.h0++;
-                                        }
-
-                                        // если слева сверху вправо вниз диагональ
-                                        if (indexfordiagonal == 0 || indexfordiagonal == 8)
-                                        {
-                                            diagonal1.h0++;
-                                        }
-
-                                        // если справа сверху влево вниз диагональ
-                                        if (indexfordiagonal == 2 || indexfordiagonal == 6)
-                                        {
-                                            diagonal2.h0++;
-                                        }
-
-                                        // если по центру ячейка
-                                        if (indexfordiagonal == 4)
-                                        {
-
-                                            diagonal1.h0++;
-                                            diagonal2.h0++;
-                                        }
-
-                                        break;
-                                }
-                                indexfordiagonal++;
+                                    break;
+                                
+                                default:
+                                    break;
                             }
-
-                            strokiList.Add(linenew);
-
-                         //   Console.WriteLine(String.Join(" ", inputsrt) + " |x:" + linenew.hX + " 0:" + linenew.h0);
+                            indexfordiagonal++;
                         }
+                        strokiList.Add(linenew);
+                    }
 
-                        stolbciList.Add(stolb0);
-                        stolbciList.Add(stolb1);
-                        stolbciList.Add(stolb2);
+                    stolbciList.Add(stolb0);
+                    stolbciList.Add(stolb1);
+                    stolbciList.Add(stolb2);
 
-                        //  ну все у нас есть структуры и есть данные . проверяем на вшивость
+                    //  ну все у нас есть структуры и есть данные . проверяем на вшивость
 
-                        var alltotalX = 0;
-                        var alltotalZero = 0;
+                    var alltotalX = 0;
+                    var alltotalZero = 0;
 
+                    for (var iterator1 = 0; iterator1 < 3; iterator1++)
+                    {
+                        alltotalX += strokiList.ElementAt(iterator1).hX;
+                        alltotalZero += strokiList.ElementAt(iterator1).h0;
+                    }
+
+                    if ((diagonal1.hX == 3) || (diagonal2.hX == 3)) // если победа то крестиков должно быть на 1 больше ноликов
+                    {
+                        if (alltotalX != alltotalZero + 1)
+                        {
+                            result = false;
+                        }
+                    }
+
+                    if ((diagonal1.h0 == 3) || (diagonal2.h0 == 3)) // если победа то ноликов должно быть как крестиков
+                    {
+                        if (alltotalZero != alltotalX)
+                        {
+                            result = false;
+                        }
+                    }
+
+                    if (result)
+                    {
                         for (var iterator1 = 0; iterator1 < 3; iterator1++)
                         {
-                            alltotalX += strokiList.ElementAt(iterator1).hX;
-                            alltotalZero += strokiList.ElementAt(iterator1).h0;
-                        }
 
-                        if ((diagonal1.hX == 3) || (diagonal2.hX == 3))
-                        {
-                            if (alltotalZero > alltotalX - 1)
+                            if (alltotalX > alltotalZero + 1)
                             {
                                 result = false;
+                                break;
                             }
-                        }
 
-                        if ((diagonal1.h0 == 3) || (diagonal2.h0 == 3))
-                        {
-                            if (alltotalZero > alltotalX)
+                            if (alltotalX < alltotalZero)
                             {
                                 result = false;
+                                break;
                             }
-                        }
 
-                        if (result)
-                        {
-                            for (var iterator1 = 0; iterator1 < 3; iterator1++)
+                            if (strokiList.ElementAt(iterator1).hX == 3) // если победа в какой-то из строк крестиками 
                             {
-
-                                if (alltotalX > alltotalZero + 1)
+                                if (alltotalX != alltotalZero + 1)
                                 {
                                     result = false;
                                     break;
                                 }
-                                else
-                                if (alltotalZero > alltotalX)
+                            }
+
+                            if (stolbciList.ElementAt(iterator1).hX == 3) // если победа в какой-то из столбцов крестиками
+                            {
+                                if (alltotalX != alltotalZero + 1)
                                 {
                                     result = false;
                                     break;
                                 }
-
-                                if (strokiList.ElementAt(iterator1).hX == 3)
-                                {
-                                    if (alltotalX == alltotalZero)
-                                    {
-                                        result = false;
-                                        break;
-                                    }
-                                }
-
-                                if (stolbciList.ElementAt(iterator1).hX == 3)
-                                {
-                                    if (alltotalX == alltotalZero)
-                                    {
-                                         result = false;
-
-                                        break;
-                                    }
-
-                                    if (alltotalX + alltotalZero >= 8)
-
-                                    {
-                                        result = false;
-                                        break;
-                                    }
-                                }
-                                if (!result)
-                                    break;
                             }
+                            if (strokiList.ElementAt(iterator1).h0 == 3) // если победа в какой-то из строк нолями 
+                            {
+                                if (alltotalX != alltotalZero) //ноли и кресты должны быть равны по количеству
+                                {
+                                    result = false;
+                                    break;
+                                }
+                            }
+
+                            if (stolbciList.ElementAt(iterator1).h0 == 3) // если победа в какой-то из столбцов крестиками
+                            {
+                                if (alltotalX != alltotalZero)
+                                {
+                                    result = false;
+                                    break;
+                                }
+                            }
+
+                            if (!result)
+                                break;
                         }
-                        Console.WriteLine(result ? "YES" : "NO");
                     }
+                    Console.WriteLine(result ? "yEs" : "nO");
                 }
             }
         }
